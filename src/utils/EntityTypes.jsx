fa8f0a0d944172c69252666340c4463f3e6cc752
Rishik Sahu <rishikkr@hotmail.com>
2024-05-15 18:23:03 +0530

    etity types updated

--- src/utils/EntityTypes.jsx
+++ src/utils/EntityTypes.jsx
@@ -49,16 +49,8 @@ export const ENTITY_TYPES = Object.freeze({
     },
     PASSWORD : {
         name: "PASSWORD",
+        icon: "fa fa-text-width",
+        dataType: "",
+        description: "",
     },
-})
-
-export const ENTITY_ICONS = Object.freeze({
-    TEXT: "fa fa-text-width",
-    CHARACTER : "fa fa-text-width",
-    BOOLEAN : "fa fa-text-width",
-    EMAIL : "fa fa-text-width",
-    NUMBER : "fa fa-text-width",
-    DATE : "fa fa-text-width",
-    TIME : "fa fa-text-width",
-    PASSWORD : "fa fa-text-width",
 })
