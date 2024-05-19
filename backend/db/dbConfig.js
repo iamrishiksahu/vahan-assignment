2c25ae84987b06bdf62f227f91fc1fb2b6a094ca
Rishik Sahu <rishikkr@hotmail.com>
2024-05-19 16:05:43 +0530

    final push

--- backend/db/dbConfig.js
+++ backend/db/dbConfig.js
@@ -1,7 +1,7 @@
 const pg = require('pg')
 const { Pool, Client } = pg
 
-const connString = process.env.DB_STRING
+const connString = 'postgres://main_xpdr_user:SA4cyyBglqDsRLKb134QgZ2oksSKOBlZ@dpg-cp4gdp0cmk4c73ek0ro0-a.oregon-postgres.render.com/main_xpdr?ssl=true'
 
 const client = new pg.Client(connString);
 client.connect();
